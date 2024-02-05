32c622b3fecaddaf6fc257388228d2f93dffefd8
Jay Stfleur <jstsaintfleur@gmail.com>
2024-02-04 21:35:37 -0800

    2 .py games added

---
+++ guess.py
@@ -0,0 +1,33 @@
+import random
+#creating guessing game
+
+#to guess number pick by computer
+def guess(x):
+    random_number = random.randint(1 ,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, Try again. Too low")
+        elif guess > random_number:
+            print("Sorry, try again. Too high")
+    print(f"Congrats. You have guessed the right number {random_number}")
+
+#function to make the computer guess a random number 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f" is {guess} too high (H), too low (L), or correct (C)??: ").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+    print(f"Nice! The computer guess your number, {guess}. correctly.")        
+computer_guess(10)
---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#Creating madlibs
+
+#Asking for user input
+word_1 = input("Text: ")
+word_2 = input("Text: ")
+word_3 = input("Text: ")
+word_4 = input("Text: ")
+
+#format string to take input
+madlibs = f"Computer programming can be really clear {word_1} and I find it interesting how the world can be {word_2}. \
+    One day I plan to travel to a city called {word_3}. When I was kid, I used to love eating {word_4}!"
+
+print(madlibs)
