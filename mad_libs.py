681be97116bbee901228af234ae60d0cfd43919a
Jean Saintfleur <jstsaintfleur@gmail.com>
2024-03-25 22:44:39 -0700

    Madlib project

---
+++ mad_libs.py
@@ -0,0 +1,14 @@
+# madlibs program
+
+# Taking input from user 
+
+random_word1 = input("Choose a random word: ")
+program = input("Choose a programming language: ")
+random_word2 = input("Choose a random word: ")
+famous_person = input("Name a famous person: ")
+country = input("Choose a country: ")
+
+madlib = f"Computer programming is so {random_word1} It makes me super excited to get to learn {program}. Every day is an opportunity to get {random_word2}. By the end of year I hope to meet {famous_person} and to also fly to {country}"
+
+# printing madlib statement
+print(madlib)
