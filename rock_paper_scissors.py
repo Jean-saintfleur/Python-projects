6d60b7211e85782de32eeb29f5f72f00170aac19
Jay Saintfleur <jaycode@Jays-MacBook-Air.local>
2024-09-26 13:11:52 -0400

    small edit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -25,7 +25,7 @@ while True:
 
     elif user_input == "paper" and computer_pick == "rock":
         print("You won!")
-        user_win += 1
+        user_wins += 1
 
     elif user_input == "scissors" and computer_pick == "paper":
         print("You won!")
