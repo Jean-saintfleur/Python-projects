e7200651048e68d37072f9b15c67f9815482ca90
Jay Saintfleur <jay@Jays-MacBook-Air.local>
2024-10-01 21:45:40 -0400

    small edit

--- adventure.py
+++ adventure.py
@@ -1,4 +1,4 @@
-name = input("Type your name: ")
+name = input("What is your name: ")
 print("Welcome", name, " to this adventure")
 
 answer = input("You are on a dirt road, it has come to and end and you can go left or right. Which way would you like to go? ").lower()
